6c5f4b424016353ebb78adf794b1bbc2a66030ae
Lh159263 <Lh159263>
2019-10-31 17:05:48 +0800

    配置消息总线

--- eureka-client/src/main/java/com/ze/eurekaclient/controller/HelloController.java
+++ eureka-client/src/main/java/com/ze/eurekaclient/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.ze.eurekaclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,14 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
  * @Desc TODO
  */
 @RestController
+@RefreshScope
 public class HelloController {
 
     @Value("${server.port}")
     String port;
 
+    @Value("${foo}")
+    String foo;
+
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     public String home(@RequestParam(value = "name", defaultValue = "forezp")  String name) {
-        return "hi " + name + " ,i am from port:" + port;
+        return "hi " + name + " ,i am from port:" + port + ",foo:" + foo;
     }
 
 
