fcdbad3143256b2e4a9291333747a41ba017d44b
Lh159263 <Lh159263>
2019-11-05 09:19:23 +0800

    完善断路器聚合监控

--- eureka-client/src/main/java/com/ze/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/ze/eurekaclient/EurekaClientApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,7 @@ import org.springframework.context.annotation.Bean;
 @EnableFeignClients
 @EnableHystrixDashboard
 @EnableCircuitBreaker
+@EnableHystrix
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
---
+++ service-lucy/src/test/java/com/ze/servicelucy/ServiceLucyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ze.servicelucy;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceLucyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
