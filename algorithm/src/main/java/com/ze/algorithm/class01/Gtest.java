b02ce6eb0a103d7ebdf3aaa83851cd7718ad0d33
1107577214 <1107577214@qq.com>
2021-02-18 23:47:04 +0800

    添加算法模块

---
+++ algorithm/src/main/java/com/ze/algorithm/class01/Gtest.java
@@ -0,0 +1,13 @@
+package com.ze.algorithm.class01;
+
+/**
+ * @Description: java类作用描述
+ * @Author: gze
+ * @CreateDate: 2021/2/18 0018 23:03
+ */
+public class Gtest
+{
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
