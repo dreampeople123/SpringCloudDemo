f81c44b41069bc61eb2a46593965d3a98d0ed50d
gaozien <Lh159263>
2021-02-19 13:52:54 +0800

    添加排序测试类

---
+++ algorithm/src/main/java/com/ze/algorithm/class01/SelectTest.java
@@ -0,0 +1,44 @@
+package com.ze.algorithm.class01;
+
+/**
+ * @Description TODO
+ * @Author Gze
+ * @Date 2020/12/29
+ */
+public class SelectTest {
+    //选择排序
+    public static void selectionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                minIndex = arr[j] < arr[i] ? j : minIndex;
+            }
+            swap(arr, i, minIndex);
+
+        }
+    }
+
+    private static void swap(int[] arr, int i, int minIndex) {
+        int tmp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = tmp;
+    }
+
+    //冒泡排序
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+
+        }
+    }
+}
