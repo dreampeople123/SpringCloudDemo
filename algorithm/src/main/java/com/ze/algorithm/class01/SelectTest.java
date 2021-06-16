123d4ec2877a3536a12f52a883921d45140df491
dreampeople123 <DreamForReal@163.com>
2021-06-16 23:00:44 +0800

    完善

--- algorithm/src/main/java/com/ze/algorithm/class01/SelectTest.java
+++ algorithm/src/main/java/com/ze/algorithm/class01/SelectTest.java
@@ -1,26 +1,28 @@
 package com.ze.algorithm.class01;
 
+import java.util.Arrays;
+
 /**
  * @Description TODO
  * @Author Gze
  * @Date 2020/12/29
  */
 public class SelectTest {
-    public static void main(String[] args) {
-
-    }
     //选择排序 O(n^2)
     public static void selectionSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
         }
-        for (int i = 0; i < arr.length - 1; i++) {
+        // 0 ~ N-1  找到最小值，在哪，放到0位置上
+        // 1 ~ n-1  找到最小值，在哪，放到1 位置上
+        // 2 ~ n-1  找到最小值，在哪，放到2 位置上
+
+        for (int i = 0; i < arr.length; i++) {
             int minIndex = i;
             for (int j = i + 1; j < arr.length; j++) {
-                minIndex = arr[j] < arr[i] ? j : minIndex;
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
             }
             swap(arr, i, minIndex);
-
         }
     }
 
@@ -41,7 +43,9 @@ public class SelectTest {
                     swap(arr, j, j + 1);
                 }
             }
+
         }
+
     }
 
     //插入排序 最坏情况：O(n^2)  最好情况：O(n) 数据情况会影响时间复杂度
@@ -54,5 +58,107 @@ public class SelectTest {
                 swap(arr, j, j + 1);
             }
         }
+    }
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            //随机生成数组
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            //复制数组
+            int[] arr2 = copyArray(arr1);
+            //排序
+            bubbleSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+
+    }
+
+    /**
+     * 数组比较
+     *
+     * @param arr1
+     * @param arr2
+     * @return
+     */
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    /**
+     * 复制数组
+     *
+     * @param arr
+     * @return
+     */
+    private static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    /**
+     * 生成随机数组
+     *
+     * @param maxSize
+     * @param maxValue
+     * @return
+     */
+    private static int[] generateRandomArray(int maxSize, int maxValue) {
+        // Math.random()   [0,1)
+        // Math.random() * N  [0,N)
+        // (int)(Math.random() * N)  [0, N-1]
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            // [-? , +?]
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
     }
 }
