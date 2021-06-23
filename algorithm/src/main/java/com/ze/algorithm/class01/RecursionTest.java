388a91be3bc889ff92f5080217909f0328a9d2dc
dreampeople123 <DreamForReal@163.com>
2021-06-24 00:07:53 +0800

    添加归并排序类

---
+++ algorithm/src/main/java/com/ze/algorithm/class01/RecursionTest.java
@@ -0,0 +1,45 @@
+package com.ze.algorithm.class01;
+
+/**
+ * 归并排序和随机快排
+ */
+public class RecursionTest {
+    //递归方法实现排序
+    public static void mergeSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process(arr, 0, arr.length - 1);
+    }
+
+    // arr[L....R]范围上，变成有序的
+    private static void process(int[] arr, int L, int R) {
+        if (L == R) {
+            return;
+        }
+        int mid = L + ((R - L) >> 1);
+        process(arr, L, mid);
+        process(arr, mid + 1, R);
+        merge(arr, L, mid, R);
+    }
+
+    //合并左右两边结果
+    private static void merge(int[] arr, int L, int M, int R) {
+        int[] help = new int[R - L + 1];
+        int i = 0;
+        int p1 = L;
+        int p2 = M + 1;
+        while (p1 <= M && p2 <= R) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= M) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= R) {
+            help[i++] = arr[p2];
+        }
+        for (int j = 0; j < help.length; j++) {
+            arr[L + j] = help[L];
+        }
+    }
+}
